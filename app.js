29f54283f9f4707c1b292d963405b12003404f81
Mostafa <anonymostafa@protonmail.ch>
2021-10-11 13:55:32 +0330

    something about url module started to be learned

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var http = require('http');
+var url = require('url');
 
 http.createServer(function (porsesh, pasokh) {
   pasokh.writeHead(200, {'Content-Type': 'text/plain'});
-  pasokh.write(porsesh.url);
-  pasokh.write(porsesh.url);
+  var q = url.parse(porsesh.url, true).query;
+  var dat3 = q.year + " " + q.month;
+  pasokh.write(dat3);
   pasokh.end();
 }).listen(8080);
 
+// http://localhost:8080/?year=2017&month=July
