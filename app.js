640f389bdfbd71d51a6257d08e8abadf940daabc
Mostafa <anonymostafa@protonmail.ch>
2021-10-11 12:54:35 +0330

    004

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var http = require('http');
 http.createServer(function (porsesh, pasokh) {
   pasokh.writeHead(200, {'Content-Type': 'text/plain'});
   pasokh.write(porsesh.url);
+  pasokh.write(porsesh.url);
   pasokh.end();
 }).listen(8080);
 
