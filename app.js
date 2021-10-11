76ba593b76f8b0437d7786ef478f9bcf6aa0e383
Mostafa <anonymostafa@protonmail.ch>
2021-10-11 15:34:58 +0330

    in this commit i learned how to append a file. including creating new ones from scratch. in the next step i am going to push a notification to the localhost:8080 :)

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function (err, data) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    return res.end();
-  });
-}).listen(5999); 
+
+fs.appendFile('a.txt', 'contents ', function (err) {
+  if (err) throw err;
+  console.log('saved :)');
+});
