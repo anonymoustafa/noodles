0ea4a51493ea1f50095b50515d03a330ee445907
Mostafa <anonymostafa@protonmail.ch>
2021-10-13 12:58:51 +0330

    Done, next challenge please

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ var http = require('http');
 var fs = require('fs');
 
 
-  http.createServer(function (req, res) {
-
-      fs.appendFile("boobs.txt","123 " ,function (err) {
-          if (err) throw err;
-
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("saved!");
-  res.end();
-});
+http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
 }).listen(8080);
