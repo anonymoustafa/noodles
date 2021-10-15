db0086b01ce73a07ec8d50462afa5274db89e1d5
Mostafa <anonymostafa@protonmail.ch>
2021-10-16 02:02:53 +0330

    this chapter is completed.

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-var fs = require('fs');
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
-http.createServer(function (req,res) {
-    var q = url.parse(req.url,true);
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
-    fs.readFile(filename,function (err,data) {
-        if (err){
-            res.writeHead(404,{'Content-Type':'text/html'})
-            res.write("not found!");
-            res.end();
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
         }
-
-        res.writeHead(200,{'Content-Type':'text/html'});
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
-        res.end();
-    })
-
-
-}).listen(1404);
-
+        return res.end();
+    });
+}).listen(8080);
